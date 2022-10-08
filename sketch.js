a5209c23e0c22fbfaaf1cd3b0d4dc8e0b88bd62e
happycherry7 <108179601+happycherry7@users.noreply.github.com>
2022-10-08 11:22:37 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	var ball;
+	var ball_options={
+		isStatic:false,
+		restitution:0.3,
+		friction:0,
+		density:1.2
+	}
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+  groundObj.display;
+  groundObj=new ground(width/2,670,width,20);
+  leftside = new ground(1100,600,20,120);
+}
+
+
+
